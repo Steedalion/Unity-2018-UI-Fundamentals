b3a21eff58663d63858ec980bd397a6c2b0a12ca
STEEDCLINT ALEX <steed@unist.ac.kr>
2021-01-12 14:13:08 +0900

    Updated slider to update volume.

---
+++ Assets/UpdateText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UpdateText : MonoBehaviour
+{
+	
+	public Text text;
+	public Slider slider;
+    
+	public void UpdateNow()
+	{
+		text.text = ""+slider.value*100f;
+	}
+}
